f0e9420abcc78daa61707eb775849598792448b3
Jeff Leverenz <jeff.leverenz@gmail.com>
2023-03-14 12:53:04 -0500

    Remove irrelevant test, re-enable injection test

--- lib/redis.module.spec.ts
+++ lib/redis.module.spec.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@nestjs/common';
 import * as Redis from 'ioredis';
 import { Test, TestingModule } from '@nestjs/testing';
 import { RedisModule } from './redis.module';
-import { getRedisConnectionToken } from './redis.utils';
 import { InjectRedis } from './redis.decorators';
 import { Redis as RedisType } from './redis.interfaces';
 
@@ -25,30 +24,7 @@ describe('RedisModule', () => {
     await app.close();
   });
 
-  it.skip('Instance Redis client provider', async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      imports: [RedisModule.forRoot({
-        config: {
-          name: '1',
-          host: '127.0.0.1',
-          port: 6379,
-          password: '123456',
-        }
-      })],
-    }).compile();
-
-    const app = module.createNestApplication();
-    await app.init();
-    const redisClient = module.get(getRedisConnectionToken('1'));
-    const redisClientTest = module.get(getRedisConnectionToken('test'));
-
-    expect(redisClient).toBeInstanceOf(Redis);
-    expect(redisClientTest).toBeInstanceOf(Redis);
-
-    await app.close();
-  });
-
-  it.skip('inject redis connection', async () => {
+  it('inject redis connection', async () => {
 
     @Injectable()
     class TestProvider {
@@ -64,7 +40,6 @@ describe('RedisModule', () => {
         config: {
           host: '127.0.0.1',
           port: 6379,
-          password: '123456',
         }
       })],
       providers: [TestProvider],
